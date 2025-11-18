b60e9c066615fb94d51b86c6e2700af8830ba6d9
rebecavasco-prog <rebeca.vasco@estudante.ifb.edu.br>
2025-11-18 16:17:09 -0300

    Add InputPassword component for password input

---
+++ components/InputPassword.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+export default function InputPassword({ placeholder }) {
+  const [senha, setSenha] = useState("");
+  return (<>
+    <input type="password" placeholder={placeholder} value={senha} onChange={e=>setSenha(e.target.value)}/>
+    {senha !== "" && <p>Senha digitada: {senha}</p>}
+  </>);
+}
